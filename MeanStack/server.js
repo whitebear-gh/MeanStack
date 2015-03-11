44c64b9db7ddad139fbc5f570d999c50606b8d0c
kizdebski <kizdebski@infusion.com>
2015-03-11 09:59:55 +0100

    after angular.js added

--- MeanStack/server.js
+++ MeanStack/server.js
@@ -24,7 +24,9 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
-
+app.get('/partials/:partialPath', function (req, res) {
+    res.render('partials/' + req.params.partialPath);
+})
 
 app.get('*', function(req, res) {
     res.render('index');
