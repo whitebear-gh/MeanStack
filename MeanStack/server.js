b817f4e411a90d4fd1c5bafa3425de52204147f7
kizdebski <kizdebski@infusion.com>
2015-03-19 10:32:32 +0100

    attached Mongo db data source

--- MeanStack/server.js
+++ MeanStack/server.js
@@ -1,6 +1,7 @@
 ﻿var express = require('express'),
     stylus = require('stylus'),
-    logger = require('morgan');
+    logger = require('morgan'),
+    mongoose = require('mongoose');
    // bodyParser = require('body-parser'); 
 
 
@@ -24,12 +25,30 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/meanstack');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error... '));
+db.once('open',function callback(){
+    console.log('DB opened');
+});
+
+var messageSchema = mongoose.Schema({ message: String });
+var Message = mongoose.model('Message',messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err,messageDoc){
+    console.log(messageDoc);
+    mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/:partialPath', function (req, res) {
     res.render('partials/' + req.params.partialPath);
 })
 
 app.get('*', function(req, res) {
-    res.render('index');
+    res.render('index',{
+        mongoMessage : mongoMessage
+    });
 });
 
 var port = 3030;
